9790e378e47b6de0e014b6ebac082f6ba8e7fd33
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2025-06-02 23:34:21 -0300

    Finish basic structure

--- juve.js
+++ juve.js
@@ -1,5 +1,24 @@
+// BOTAO PARA TOGGLE DO MENU
 const menuToggle = document.querySelector('.menu-toggle');
 const navegation = document.querySelector('.navegation');
 menuToggle.addEventListener('click', () => {
     navegation.classList.toggle('open');
 });
+
+// SCROLL PARA OCULTAR/EXIBIR O HEADER
+let lastScrollTop = 0;
+    const header = document.querySelector("header");
+
+    window.addEventListener("scroll", () => {
+        const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (currentScroll > lastScrollTop) {
+            // Scroll para baixo
+            header.classList.add("hide");
+        } else {
+            // Scroll para cima
+            header.classList.remove("hide");
+        }
+
+        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Evita valores negativos
+    });
