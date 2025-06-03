f9b4fc60bedb2917ed8ea9ab82bb8bab374d9f57
Daniel Ribeiro <109773063+DevJuvenilson@users.noreply.github.com>
2025-06-03 18:24:21 -0300

    Rename files

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+// BOTAO PARA TOGGLE DO MENU
+const menuToggle = document.querySelector('.menu-toggle');
+const navegation = document.querySelector('.navegation');
+menuToggle.addEventListener('click', () => {
+    navegation.classList.toggle('open');
+});
 
+// SCROLL PARA OCULTAR/EXIBIR O HEADER
+let lastScrollTop = 0;
+const header = document.querySelector("header");
 
-const myObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry)=>{
-        if (entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.remove('show') 
-        }
-    })
-})
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
 
-const elements = document.querySelectorAll('.hidden')
+    if (currentScroll > lastScrollTop) {
+        // Scroll para baixo
+        header.classList.add("hide");
+    } else {
+        // Scroll para cima
+        header.classList.remove("hide");
+    }
 
-elements.forEach((element)=> myObserver.observe(element))
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Evita valores negativos
+});
+
+// CARROSSEL DE IMAGENS
+const leftBtn = document.querySelector(".carrossel-btn.left");
+const rightBtn = document.querySelector(".carrossel-btn.right");
+const carrosselImages = document.querySelector(".carrossel-images");
+
+leftBtn.addEventListener("click", () => {
+    carrosselImages.scrollBy({
+        left: -220, // Largura do card + gap
+        behavior: "smooth"
+    });
+});
+
+rightBtn.addEventListener("click", () => {
+    carrosselImages.scrollBy({
+        left: 220,
+        behavior: "smooth"
+    });
+});
